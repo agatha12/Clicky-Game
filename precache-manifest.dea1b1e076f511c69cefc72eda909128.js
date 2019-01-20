5aeb0b1d89fb7bc40f32c63eb819ee85d2d4ba32
agatha12 <agkiecun@gmail.com>
2019-01-19 22:11:11 -0500

    Updates

---
+++ precache-manifest.dea1b1e076f511c69cefc72eda909128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "1d99269fbfb998217750",
+    "url": "/Clicky-Game/static/js/main.1d99269f.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/Clicky-Game/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "1d99269fbfb998217750",
+    "url": "/Clicky-Game/static/css/main.0ba6cb51.chunk.css"
+  },
+  {
+    "revision": "19e4669eece63decbdb1418dd0af26c9",
+    "url": "/Clicky-Game/index.html"
+  }
+];
